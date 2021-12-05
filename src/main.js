ab84db77a312ac8123e2941dea014af681375771
glazunovden@yandex.ru <27111993Den>
2021-12-05 13:07:26 +0300

    2.10. Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -8,27 +8,65 @@ import {createPopupTemplate} from './view/film-details-view.js';
 import {createFooterStatisticsTemplate} from './view/statistics-view.js';
 
 import {renderTemplate, RenderPosition} from './render.js';
+import {generateMovie} from './mock/movie';
+import {generateComment} from './mock/comment';
+import {getRandomInteger} from './utils';
 
-const FILM_CARD_COUNT = 5;
+const FILM_CARD_COUNT = 20;
+const FILM_COUNT_PER_STEP = 5;
+
+
+const films = Array.from({length: FILM_CARD_COUNT}, generateMovie);
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
+const footerStatisticsElement = document.querySelector('.footer__statistics');
 
 renderTemplate(siteHeaderElement, createProfileTemplate(), RenderPosition.BEFOREEND);
-renderTemplate(siteMainElement, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createSiteMenuTemplate(films), RenderPosition.BEFOREEND);
 renderTemplate(siteMainElement, createSortLinksTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(siteMainElement, createFilmsSectionTemplate(), RenderPosition.BEFOREEND);
-
+renderTemplate(footerStatisticsElement, createFooterStatisticsTemplate(films), RenderPosition.BEFOREEND);
 
 const filmsContainerElement = document.querySelector('.films-list__container');
 
-for (let i = 0; i < FILM_CARD_COUNT; i++) {
-  renderTemplate(filmsContainerElement, createFilmCardTemplate(), RenderPosition.BEFOREEND);
+function generateComments (arr) {
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const randNum = getRandomInteger(0, 5);
+
+    for (let j = 0; j < randNum; j++) {
+      newArr.push(generateComment(arr[i].idx));
+    }
+  }
+  return newArr;
+}
+const comments = generateComments(films);
+
+for (let i = 0; i < Math.min(films.length, FILM_COUNT_PER_STEP); i++) {
+  renderTemplate(filmsContainerElement, createFilmCardTemplate(films[i], comments), RenderPosition.BEFOREEND);
 }
 
-renderTemplate(filmsContainerElement, createButtonMoreTemplate(), RenderPosition.AFTEREND);
-renderTemplate(siteMainElement, createPopupTemplate(), RenderPosition.AFTEREND);
+if (films.length > FILM_COUNT_PER_STEP) {
+  let renderedTaskCount = FILM_COUNT_PER_STEP;
 
-const footerStatisticsElement = document.querySelector('.footer__statistics');
+  renderTemplate(filmsContainerElement, createButtonMoreTemplate(), RenderPosition.AFTEREND);
 
-renderTemplate(footerStatisticsElement, createFooterStatisticsTemplate(), RenderPosition.BEFOREEND);
+  const loadMoreButton = document.querySelector('.films-list__show-more');
+
+  loadMoreButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    films
+      .slice(renderedTaskCount, renderedTaskCount + FILM_COUNT_PER_STEP)
+      .forEach((film) => renderTemplate(filmsContainerElement, createFilmCardTemplate(film, comments), RenderPosition.BEFOREEND));
+
+    renderedTaskCount += FILM_COUNT_PER_STEP;
+
+    if (renderedTaskCount >= films.length) {
+      loadMoreButton.remove();
+    }
+  });
+}
+
+renderTemplate(siteMainElement, createPopupTemplate(films[0], comments), RenderPosition.AFTEREND);
---
+++ src/mock/movie.js
@@ -0,0 +1,56 @@
+import {
+  getRandomInteger,
+  getRandomFloat,
+  generateRandomString,
+  generateRandomMuchString, generateRuntime, generateDate
+} from '../utils.js';
+
+import {
+  DEFAULT_MIN_NUMBER,
+  FILMS_ACTORS, FILMS_COUNTRIES,
+  FILMS_DESCRIPTIONS, FILMS_DIRECTORS,
+  FILMS_GENRES,
+  FILMS_POSTERS,
+  FILMS_SCREEN_WRITERS,
+  FILMS_TITLES,
+  MAX_RATING_NUMBER,
+  MIN_RATING_NUMBER,
+  RATING_DECIMAL,
+  DEFAULT_MAX_NUMBER,
+  MAX_GENRES_COUNT,
+  MAX_DESCRIPTIONS_COUNT,
+  MAX_AGE,
+} from './constants';
+
+export const generateMovie = () => {
+
+  const runTime = generateRuntime().format('H[h] m[m]');
+  const releaseDate = generateDate().format('DD MMMM YYYY');
+  const releaseDateYear = generateDate().format('YYYY');
+  const genres = generateRandomMuchString(FILMS_GENRES, MAX_GENRES_COUNT);
+  const description = generateRandomMuchString(FILMS_DESCRIPTIONS, MAX_DESCRIPTIONS_COUNT);
+  const writers = generateRandomMuchString(FILMS_SCREEN_WRITERS, MAX_DESCRIPTIONS_COUNT).join(', ');
+  const actors = generateRandomMuchString(FILMS_ACTORS, MAX_DESCRIPTIONS_COUNT).join(', ');
+  const idx = Math.random().toString(10).substr(2, 9);
+
+  return {
+    idx,
+    title: generateRandomString(FILMS_TITLES),
+    releaseDate,
+    releaseDateYear,
+    description: description.join(' '),
+    runTime,
+    genres,
+    genresOne: generateRandomString(genres),
+    isWatchlist: Boolean(getRandomInteger(DEFAULT_MIN_NUMBER, DEFAULT_MAX_NUMBER)),
+    isWatched: Boolean(getRandomInteger(DEFAULT_MIN_NUMBER, DEFAULT_MAX_NUMBER)),
+    isFavorite: Boolean(getRandomInteger(DEFAULT_MIN_NUMBER, DEFAULT_MAX_NUMBER)),
+    image: generateRandomString(FILMS_POSTERS),
+    rating: getRandomFloat(MIN_RATING_NUMBER, MAX_RATING_NUMBER, RATING_DECIMAL),
+    writers,
+    director: generateRandomString(FILMS_DIRECTORS),
+    actors,
+    country: generateRandomString(FILMS_COUNTRIES),
+    age: getRandomInteger(DEFAULT_MIN_NUMBER, MAX_AGE)
+  };
+};
